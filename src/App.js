041594b8d080d94069551cd4de5b4d55a921dc77
gnragazzi <gnragazzi@gmail.com>
2021-03-24 12:36:06 -0300

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 function App() {
+  const str = `# A Markdown Header
+
+## A Markdown Sub Header
+
+A Markdown [link](https://www.freecodecamp.org)
+
+A Markdown InnerCode, \`<p>Hello World!</p>\`
+
+\`\`\`
+// A Markdown block code:
+
+console.log('Hello World')
+
+\`\`\`
+
+- A Markdown List-Item
+
+> A Markdown blokquote
+
+A Markdown Image: ![image](https://images.pexels.com/photos/1767434/pexels-photo-1767434.jpeg?cs=srgb&dl=pexels-j-u-n-e-1767434.jpg&fm=jpg)
+
+A Markdown **bold text**
+
+Thats all!
+
+Thanks for watching :-)
+
+`
+  const [markdown, setMarkdown] = useState(str)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='section-center'>
+      <textarea
+        id='editor'
+        className='markdown-editor'
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      ></textarea>
+      <article id='preview'>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </article>
+    </section>
+  )
 }
 
-export default App;
+export default App
