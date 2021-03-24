8d1823ff2a881ab4a00d3942d70b7a4960bbb03c
gnragazzi <gnragazzi@gmail.com>
2021-03-24 15:20:53 -0300

    now styled!

--- src/App.js
+++ src/App.js
@@ -32,17 +32,37 @@ Thanks for watching :-)
 `
   const [markdown, setMarkdown] = useState(str)
   return (
-    <section className='section-center'>
-      <textarea
-        id='editor'
-        className='markdown-editor'
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-      ></textarea>
-      <article id='preview'>
-        <ReactMarkdown>{markdown}</ReactMarkdown>
-      </article>
-    </section>
+    <main>
+      <section className='section-center'>
+        <h1 className='title'>markdown previewer</h1>
+        <div className='markdown-container'>
+          <div className='markdown-article'>
+            <h2 className='markdown-subtitle'>editor</h2>
+            <textarea
+              id='editor'
+              value={markdown}
+              onChange={(e) => setMarkdown(e.target.value)}
+            ></textarea>
+          </div>
+          <div className='markdown-article'>
+            <h2 className='markdown-subtitle'>preview</h2>
+            <article id='preview'>
+              <ReactMarkdown>{markdown}</ReactMarkdown>
+            </article>
+          </div>
+        </div>
+      </section>
+      <footer className='footer'>
+        <p className='footer-text'>
+          {new Date().getFullYear()} Markdown Previewer.<br></br> This is a
+          project for
+          <a href='https://www.freecodecamp.org/' target='_blank'>
+            Free Code Camp
+          </a>
+          . Check Out this amaizing free site to learn how to code.
+        </p>
+      </footer>
+    </main>
   )
 }
 
